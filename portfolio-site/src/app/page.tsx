8b4ea422e2d852fa0a00152f889ed122685bdc11
Tayyaba Ramzan <160269220+Tayyaba-Ramzan@users.noreply.github.com>
2025-03-26 18:18:15 -0700

    Update page.tsx

--- portfolio-site/src/app/page.tsx
+++ portfolio-site/src/app/page.tsx
@@ -1,7 +1,7 @@
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Achievement from "@/components/Achievement";
+import Achievement from "@/components/Achievement"
 export default function Home(){
   return(
     <>
